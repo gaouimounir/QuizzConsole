99c45b3559cd9dcc88687ea6536c50f79c23a4c6
gaouimounir <mounir.gaoui@outlook.com>
2023-11-28 12:16:51 +0100

    creation des methodes

--- Program.cs
+++ Program.cs
@@ -12,16 +12,44 @@
 
         }
 
-        private static void ParcourirQuestions()
-        {
-
-        }
-
         static void AccueilJoueur()
         {
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
             Console.WriteLine($"{Environment.NewLine}Welcome, {name}!");
         }
+
+        private static void ParcourirQuestions()
+        {
+
+            PoserQuestion();
+            DonnerReponse();
+            VerifierReponse();
+            AfficherScore();
+
+        }
+
+
+        private static void PoserQuestion()
+        {
+
+        }
+
+        private static void DonnerReponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void VerifierReponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AfficherScore()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
