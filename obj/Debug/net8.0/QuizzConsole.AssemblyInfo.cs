01da7e97f781e42add64678e8e2b8935acafa6a5
gaouimounir <mounir.gaoui@outlook.com>
2023-11-29 16:45:07 +0100

    debug

--- obj/Debug/net8.0/QuizzConsole.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuizzConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizzConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d351f2d6f50cbdebce016e8aeb7b9d081f341108")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizzConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizzConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
